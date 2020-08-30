67b29df9166cc79efb6cc6ffe99141bf91103ff0
Valeria Duran de Ruiz <valeria.do@gmail.com>
2020-08-30 16:40:21 -0500

    add documentation and initial card work

--- 3ThingsIOS/ContentView.swift
+++ 3ThingsIOS/ContentView.swift
@@ -6,11 +6,48 @@
 //  Copyright © 2020 Valeria Duran Ruiz. All rights reserved.
 //
 
+/* BUILD a card that includes
+ TOP
+  - a card view - rounded corners
+        * 3 LABELS  - to hold the 3 LOVES of the day
+ 
+  - TEXTFIELD - to enter loves
+  
+   //probably do  not add button
+ 
+ */
+//
+
 import SwiftUI
 
 struct ContentView: View {
+//    @State private var threeLovesText = ""
+//     @State private var threeLovesText = ""
+//     @State private var threeLovesText = ""
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+//             Text("\(threeLovesText)")
+//
+//            TextField("What did you love today", text: $threeLovesText)
+//            TextField("What did you fear today", text: $threeLovesText)
+//            TextField("What did you desire today", text: $threeLovesText)
+           CardView() //3 THINGS YOU LOVE
+            CardView() //LOVE fear desire
+        }
+    }
+}
+//make sure it is a changeable card view - like in vapours
+struct CardView: View {
+    @State private var inputText1 = ""
+    @State private var inputText2 = ""
+    @State private var inputText3 = ""
+    var body: some View {
+        VStack {
+            TextField("What did you love today", text: $inputText1)
+            TextField("What did you fear today", text: $inputText2)
+            TextField("What did you desire today", text: $inputText3)
+        }
     }
 }
 
